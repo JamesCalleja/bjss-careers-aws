a589405a532b12c2071c3b5c9723c58f82ccc1a2
JamesCalleja <james.calleja@gmail.com>
2025-01-27 13:03:16 +0000

    adding some suggestions

--- bootstrap/terraform/locals.tf
+++ bootstrap/terraform/locals.tf
@@ -2,10 +2,10 @@ locals {
   prefix = "${var.project}-${var.component}"
 
   uppercase_project       = upper(var.project)
-  account_admin_role_name = "aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_Administrator_13edb3b19d3fd0cc"
+  account_admin_role_name = "aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_Administrator_13edb3b19d3fd0cc" #TODO: This should not be hardcoded
 
   deployment_default_tags = {
-    AccountId   = var.aws_account_id
+    AccountId   = data.aws_caller_identity.current.account_id
     AccountName = "bjss-careers"
     Project     = var.project
     Component   = var.component
